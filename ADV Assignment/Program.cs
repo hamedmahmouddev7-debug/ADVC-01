aa0bd2d082a87ff3e5e210c67599a84d092377b9
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-09-01 13:39:50 +0300

    Question06

--- ADV Assignment/Program.cs
+++ ADV Assignment/Program.cs
@@ -54,6 +54,24 @@ namespace ADV01
                   Console.WriteLine(y);
              */
             #endregion
+
+            #region Question05
+            // Write a generic method FindMax<T> that finds maximum value 
+
+            /*
+             * static T FindMax<T>(T a, T b) where T : IComparable<T>
+               {
+                   if (a.CompareTo(b) > 0)
+                       return a;
+                   else
+                       return b;
+               }
+              int max = FindMax(10, 20);
+
+              Console.WriteLine(max);
+              */
+
+            #endregion
         }
     }
 }
