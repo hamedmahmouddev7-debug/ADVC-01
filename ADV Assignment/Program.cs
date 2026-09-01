6f6b55c9840dab43a6b5a3d1e7601bd053e97482
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-09-01 13:26:50 +0300

    Question02

--- ADV Assignment/Program.cs
+++ ADV Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ADV01
+﻿using ADV_Assignment;
+
+namespace ADV01
 {
     class Program
     {
@@ -8,6 +10,19 @@
             // What is a generic class? Why use generics? 
 
             //Generics allow you to write one reusable class or method that works with different data types safely.
+            #endregion
+
+            #region Question02
+            // Write a generic class Container<T> with Add and Get methods
+
+            /*
+             Container<int> container = new Container<int>();
+
+                  container.Add(10);
+
+                   Console.WriteLine(container.Get());
+             
+            */
             #endregion
         }
     }
